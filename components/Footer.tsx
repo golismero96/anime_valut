b024e38b951945ad2e2dc95bd322e9909509d40d
m.dadfar <golismero96@gmail.com>
2023-12-02 17:06:32 +0330

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+function Footer() {
+  return (
+    <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
+      <p className="text-base font-bold text-white">@2023 EpicAnimeVault</p>
+      <Image
+        src="./assets/shape/svgs/logo.svg"
+        alt="logo"
+        width={47}
+        height={44}
+        className="object-contain"
+      />
+      <div className="flex items-center gap-6">
+        <Image
+          src="./assets/shape/svgs/tiktok.svg"
+          alt="logo"
+          width={19}
+          height={19}
+          className="object-contain"
+        />
+        <Image
+          src="./assets/shape/svgs/instagram.svg"
+          alt="logo"
+          width={19}
+          height={19}
+          className="object-contain"
+        />
+        <Image
+          src="./assets/shape/svgs/twitter.svg"
+          alt="logo"
+          width={19}
+          height={19}
+          className="object-contain"
+        />
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
