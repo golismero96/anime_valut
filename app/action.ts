ee3b0eac1a17a8ce18f02a11128b7f1b8e4656f6
m.dadfar <golismero96@gmail.com>
2023-12-06 17:43:39 +0330

    infinit scroll

--- app/action.ts
+++ app/action.ts
@@ -2,12 +2,10 @@
 
 export const fetchAnime = async (page: number) => {
   const response = await fetch(
-    "https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity"
+    `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`
   );
 
   const data = await response.json();
-
-  console.log(data);
 
   return data;
 };
